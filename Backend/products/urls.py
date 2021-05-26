43d41782c74011c4ddc3f472e4d95553cbb58cf6
Shashwat-Nandan <shashwat.nandan@propelytics.in>
2021-05-26 20:35:42 +0530

    delete old unused folders

--- Backend/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import SimpleRouter
-from .views import ProductViewSet, StoreViewSet
-
-router = SimpleRouter()
-router.register("products", ProductViewSet, basename='products')
-router.register("", StoreViewSet, basename='stores')
-
-urlpatterns = router.urls
